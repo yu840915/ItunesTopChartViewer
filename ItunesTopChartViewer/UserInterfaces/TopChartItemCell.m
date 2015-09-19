9aa50348c746a50a73e745e0f9cc12f528759fe1
LihsuanYu <yu840915@gmail.com>
2015-09-19 13:31:23 +0800

    Create ui classes

---
+++ ItunesTopChartViewer/UserInterfaces/TopChartItemCell.m
@@ -0,0 +1,13 @@
+//
+//  TopChartItemCell.m
+//  ItunesTopChartViewer
+//
+//  Created by small on 9/19/15.
+//  Copyright © 2015 YLS. All rights reserved.
+//
+
+#import "TopChartItemCell.h"
+
+@implementation TopChartItemCell
+
+@end
---
+++ ItunesTopChartViewer/UserInterfaces/TopChartViewController.m
@@ -0,0 +1,85 @@
+//
+//  TopChartViewController.m
+//  ItunesTopChartViewer
+//
+//  Created by small on 9/19/15.
+//  Copyright © 2015 YLS. All rights reserved.
+//
+
+#import "TopChartViewController.h"
+
+@interface TopChartViewController ()
+
+@end
+
+@implementation TopChartViewController
+
+static NSString * const reuseIdentifier = @"TopChartItemCellID";
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+#pragma mark <UICollectionViewDataSource>
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 20;
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell
+    
+    return cell;
+}
+
+#pragma mark <UICollectionViewDelegate>
+
+/*
+// Uncomment this method to specify if the specified item should be highlighted during tracking
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath {
+	return YES;
+}
+*/
+
+/*
+// Uncomment this method to specify if the specified item should be selected
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+*/
+
+/*
+// Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldShowMenuForItemAtIndexPath:(NSIndexPath *)indexPath {
+	return NO;
+}
+
+- (BOOL)collectionView:(UICollectionView *)collectionView canPerformAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
+	return NO;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView performAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
+	
+}
+*/
+
+@end
