183866435895eaaee00af2c26f9936312aece02e
LihsuanYu <yu840915@gmail.com>
2015-09-19 12:39:37 +0800

    Initial Commit

---
+++ ItunesTopChartViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ItunesTopChartViewer
+//
+//  Created by small on 9/19/15.
+//  Copyright © 2015 YLS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ItunesTopChartViewer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ItunesTopChartViewer
+//
+//  Created by small on 9/19/15.
+//  Copyright © 2015 YLS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
